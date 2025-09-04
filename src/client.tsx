c65bc0014e2227ccb3ee2a97993bd9bc053d17d7
Jakob Norlin <jakob.stahl91@gmail.com>
2025-09-04 19:11:26 +0200

    Reproduce issue

---
+++ src/client.tsx
@@ -0,0 +1,7 @@
+import { StartClient } from "@tanstack/solid-start"
+import { hydrate } from "solid-js/web"
+import { createRouter } from "./router"
+
+const router = createRouter()
+
+hydrate(() => <StartClient router={router} />, document)
